149d32c342b6e77d78f35c52a96dfc4c926e405c
madhav-codehub <madhav.p.akv@gmail.com>
2026-08-15 16:17:51 +0530

    api: added new api called wish

--- src/main/java/com/luv4code/controller/HelloController.java
+++ src/main/java/com/luv4code/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.luv4code.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class HelloController {
     @GetMapping("/hello")
     public ResponseEntity<String> wish() {
         return ResponseEntity.ok("Hello CI/CD with Jenkins");
+    }
+
+    @GetMapping("/greet/{message}")
+    public ResponseEntity<String> greet(@PathVariable String message){
+        return ResponseEntity.ok("Hello, "+message);
     }
 
 }
